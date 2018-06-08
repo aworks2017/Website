b57454cf564469a7309ecad63714f827739b8574
Joe Badrov <joe.badrov@gmail.com>
2018-06-07 22:39:01 -0500

    another test

--- unlink_screenshot_submission_files.php
+++ unlink_screenshot_submission_files.php
@@ -5,6 +5,7 @@ $squantial_number = $wpdb->get_results("SELECT files_path FROM ".$wpdb->prefix."
 foreach($squantial_number as $file_path){
 	deleteOldFiles('wp-admin/'.$file_path->files_path);
 }
+
 echo "All screen shot submitted files before seven days were successfully  deleted.";
 
 function deleteOldFiles($targetfolder) {
