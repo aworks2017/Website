6403210dcce0f1926631dc23b1f593a13ac9bd54
Joe Badrov <joe.badrov@gmail.com>
2019-03-16 23:24:37 -0500

    Testing CI build

--- wp-content/themes/formationpro-child/functions.php
+++ wp-content/themes/formationpro-child/functions.php
@@ -4,6 +4,7 @@ function my_theme_enqueue_styles() {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
     /*
+
     $parent_style = 'parent-style';
 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
@@ -12,6 +13,7 @@ function my_theme_enqueue_styles() {
         array( $parent_style ),
         wp_get_theme()->get('Version')
     );
+
     */
 }
 
